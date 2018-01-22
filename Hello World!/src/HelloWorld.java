1f901a5feb8e4196208dac994a8241ea788a95ad
Will <will.green98@hotmail.com>
2018-01-22 12:16:11 +0000

    Hello World!

--- Hello World!/src/HelloWorld.java
+++ Hello World!/src/HelloWorld.java
@@ -2,6 +2,7 @@
  * @author Will
  * This is the beginning to my Java Programming at Uni
  */
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
