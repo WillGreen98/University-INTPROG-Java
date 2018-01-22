de3d1d1626f18b5dd6ba5b1d5888b98609acb6c5
Will <will.green98@hotmail.com>
2018-01-22 12:20:04 +0000

    InClass Package

--- T2 Week 15/src/inClass/InClass_Week15.java
+++ T2 Week 15/src/inClass/InClass_Week15.java
@@ -1,10 +1,12 @@
- /**
- * @author Will
- * In Class Code - Examples
- */
+package inClass;
 
+/**
+ * @author Will
+ * In Class Week 15 - Examples
+ */
 public class InClass_Week15 {
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
+
 	}
 }
