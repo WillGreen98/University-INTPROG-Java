24dcbaba0b676c15ce01483c43f95f126ac3d3cf
Will <will.green98@hotmail.com>
2018-01-22 12:42:20 +0000

    IsWillHavingFun = True

--- Hello World!/src/HelloWorld.java
+++ Hello World!/src/HelloWorld.java
@@ -3,8 +3,32 @@
  * This is the beginning to my Java Programming at Uni
  */
 
+import java.util.Random;
+
 public class HelloWorld {
+	static boolean isWillHavingFun;
+	static boolean isWillLearning = false;
+	
+	Random rand = new Random();
+	
 	public static void main(String[] args) {
+		int low = Integer.parseInt(args[0]);
+        int high = Integer.parseInt(args[1]);
+        
+		int rand_Num = (int) (Math.random() * (high - low)) + low;
+		
 		System.out.println("Hello World!");
+		
+		if(rand_Num == 1) {
+			isWillHavingFun = true;
+			System.out.println("Will is having fun");
+		} else {
+			if(isWillLearning = false) {
+				isWillHavingFun = true;
+			}
+			
+			isWillHavingFun = false;
+			System.out.println("Will is Bored");
+		}	
 	}
 }
