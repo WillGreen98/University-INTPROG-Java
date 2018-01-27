49727edf67463fec418a097de174ce1f876ed030
Will <will.green98@hotmail.com>
2018-01-27 13:12:13 +0000

    Week 15 Update

--- T2 Week 15/src/inClass/Car.java
+++ T2 Week 15/src/inClass/Car.java
@@ -1,13 +1,11 @@
+package inClass;
 
 /**
- * A class to model a Car.
- * 
- * @author Claire Ancient 
- * @version 1.0
+ * @author Will
+ * In Class Week 15
  */
-public class Car
-{
-    // instance variables
+
+public class Car {
     String make;
     String model;
     String registration;
@@ -19,22 +17,7 @@ public class Car
     boolean airConOn;
     boolean lightsOn;
 
-
-
-    /**
-     * Constructor for objects of class Car
-     * 
-     * @param cMake the make of the car
-     * @param cModel the model of the car
-     * @param cReg the car registration
-     * @param cEngine the engine of the car
-     * @param cFuel the fuel used by the car
-     * @param cDriver the driver side of the car
-     * 
-     */
-    public Car(String cMake, String cModel, String cReg, double cEngine, String cFuel, String cDriver)
-    {
-        // instance variables initialisation
+    public Car(String cMake, String cModel, String cReg, double cEngine, String cFuel, String cDriver) {
         make = cMake;
         model = cModel;
         registration = cReg;
@@ -47,36 +30,17 @@ public class Car
         lightsOn = false;
     }
 
-    /**
-     * A method to change the registration number
-     * 
-     * @param  newReg   the new registration number for the car 
-     */
-    public void changeRegistration(String newReg)
-    {
+    public void changeRegistration(String newReg) {
         registration = newReg;
     }
     
-    /**
-     * A method to get the number of miles until the next service is due (assuming that there is a service every 12,000 miles)
-     * 
-     * @return the number of miles until the next service
-     */
-    public double milesToNextService()
-    {
+    public double milesToNextService() {
         double milesLeft = 12000 - (currentMileage % 12000);
         
         return milesLeft;
     }
-    
-    /**
-     * A method to add mileage to the car through driving (this method will be changed when we have done for loops)
-     * 
-     * @param miles number of miles to be driven
-     * @return mileage of the car after driving
-     */
-    public double drive(double miles)
-    {
+
+    public double drive(double miles) {
         currentMileage += miles;
         
         return currentMileage;
--- T2 Week 15/src/inClass/LecturerDirectory.java
+++ T2 Week 15/src/inClass/LecturerDirectory.java
@@ -1,15 +1,17 @@
+package inClass;
 
-public class LecturerDirectory
-{
-    // instance variables
+/**
+ * @author Will
+ * In Class Week 15
+ */
+
+public class LecturerDirectory {
     private String name;
     private int phoneNumber;
     private String office;
     private int numProjectStudents;
 
-    // constructor
-    public LecturerDirectory(String lecturerName, int lecturerPhone, String officeNo)
-    {
+    public LecturerDirectory(String lecturerName, int lecturerPhone, String officeNo) {
         // initialise instance variables
         name = lecturerName;
         phoneNumber = lecturerPhone;
@@ -17,15 +19,11 @@ public class LecturerDirectory
         numProjectStudents = 0;
     }
 
-    // method to add a project student
-    public void addProjectStudent()
-    {
+    public void addProjectStudent() {
         numProjectStudents += 1;
     }
     
-    // method to calculate num hours devoted to project students based on the number of weeks entered (assume 2 hours per week per student)
-    public int calculateProjectStudentHours(int numWeeks)
-    {
+    public int calculateProjectStudentHours(int numWeeks) {
         int numProjectHours = (numProjectStudents * 2) * numWeeks;
         
         return numProjectHours;
