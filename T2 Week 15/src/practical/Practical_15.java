a35ed547cf67453f4489cb7d96eb07362cca20ef
Will <will.green98@hotmail.com>
2018-01-29 11:53:28 +0000

    Week 15 Practical Update

--- T2 Week 15/src/practical/Practical_15.java
+++ T2 Week 15/src/practical/Practical_15.java
@@ -7,6 +7,6 @@ package practical;
 
 public class Practical_15 {
 	public static void main(String[] args) {
-
+		System.out.println("This week's practical was VV interesting... It was about learning how to use BlueJ");
 	}
 }
