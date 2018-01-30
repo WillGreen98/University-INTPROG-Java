ce52c0446c9bcfe1ae1bc7536369f67d11bfea60
Will <will.green98@hotmail.com>
2018-01-30 13:37:35 +0000

    Week 16 Lecture 2 :game_die: Upload

---
+++ T2 Week 16/src/dice/Dice.java
@@ -0,0 +1,32 @@
+package dice;
+
+/**
+ * @author Will
+ * Week 16 - Lecture 2
+ */
+
+import java.util.Random;
+
+public class Dice {
+	Random random = new Random();
+	
+	public Dice() {
+		// Kinda useless... We only used class Dice to hold Random();
+	}
+	
+	public int throwDice() {
+		int roll = random.nextInt(6);
+		
+		return roll + 1;
+	} 
+	
+	
+	public static void main(String[] args) {
+		Dice dice = new Dice();
+		
+		for(int i=0; i <= 10; i++) {
+			int die_Throw = dice.throwDice();
+			System.out.println(die_Throw);
+		}
+	}
+}
