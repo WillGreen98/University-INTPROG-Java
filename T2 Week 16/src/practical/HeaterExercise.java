5087df03967b415f049b08b6dff188bb53feb1db
Will <will.green98@hotmail.com>
2018-02-02 16:39:53 +0000

    Week 16 Task 6 Completed

--- T2 Week 16/src/practical/HeaterExercise.java
+++ T2 Week 16/src/practical/HeaterExercise.java
@@ -7,30 +7,59 @@ package practical;
 
 public class HeaterExercise {
 	private float temperature;
-	private int adapation_temp = 5;
+	private int adapation_Temp = 5;
 	
-	public HeaterExercise() {
+	private float min_Temp;
+	private float max_Temp;
+	
+	public HeaterExercise(float cTemperature, float cMin, float cMax) {
 		temperature = 15.0f;
+		min_Temp = cMin;
+		max_Temp = cMax;
 	}
 	
+	public float getMin_Temp() {
+		return min_Temp;
+	}
+
+	public void setMin_Temp(float min_Temp) {
+		this.min_Temp = min_Temp;
+	}
+
+	public float getMax_Temp() {
+		return max_Temp;
+	}
+
+	public void setMax_Temp(float max_Temp) {
+		this.max_Temp = max_Temp;
+	}
+
 	public int getAdapt() {
-		return adapation_temp;
+		return adapation_Temp;
 	} 
-	
+		
 	public void setAdapt(float newAdapt) {
 		if(newAdapt <= 0) {
 			System.out.println("You cannot set value to zero or below");
 		} else {
-			adapation_temp += newAdapt;
+			adapation_Temp += newAdapt;
 		}
 	} 
 	
 	public void cooler() {
-		temperature -= getAdapt();
+		if(getAdapt() < max_Temp && getAdapt() > min_Temp) {
+			temperature -= getAdapt();
+		} else {
+			System.out.println("Error");
+		}
 	} 
-	
+		
 	public void warmer() {
-		temperature += getAdapt();
+		if(getAdapt() < max_Temp && getAdapt() > min_Temp) {
+			temperature += getAdapt();
+		} else {
+			System.out.println("Error");
+		}
 	} 
 	
 	public float getTemp() {
@@ -38,7 +67,7 @@ public class HeaterExercise {
 	} 
 	
 	public static void main(String[] args) {
-		HeaterExercise heater = new HeaterExercise();
+		HeaterExercise heater = new HeaterExercise(15.0f, -10.0f, 92.0f);
 		System.out.println(heater.getTemp());
 		
 		heater.warmer();
