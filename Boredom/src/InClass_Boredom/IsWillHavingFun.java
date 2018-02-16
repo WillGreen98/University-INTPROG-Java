7bccd1124fdbba3372e05f610a601cc5119005cb
Will <will.green98@hotmail.com>
2018-02-16 10:43:47 +0000

    Deletion of unused code

--- Boredom/src/InClass_Boredom/IsWillHavingFun.java
+++ Boredom/src/InClass_Boredom/IsWillHavingFun.java
@@ -15,29 +15,11 @@ public class IsWillHavingFun {
 	static boolean isWillHavingFun;
 	static boolean isWillLearning = false;
 	
-	public static boolean rand_Number() {
-		int low = 0;
-        int high = 1;
-        
-		int rand_Num = (int)(Math.random() * (high - low)) + low;
-		
-		if(rand_Num == 1) {
-			isWillHavingFun = true;
-			System.out.println("Will is having fun");
-		} else {
-			if(isWillLearning = false) {
-				isWillHavingFun = true;
-			}
-			
-			isWillHavingFun = false;
-			System.out.println("Will is Bored");
-		}	
-		
-		return isWillHavingFun;
-	} 
-
 	public static void main(String[] args) {
-		Calendar c = new GregorianCalendar();
-	    System.out.println(Locale.getDefault() + ": " + (c.getFirstDayOfWeek() - 1));   
+		Calendar_Att cal_Att = new Calendar_Att();
+		Calendar gCal = new GregorianCalendar();
+		
+		
+	    System.out.println(Locale.getDefault() + ": " + (gCal.getFirstDayOfWeek() - 1) + " " + cal_Att.hourOfDay);   
 	}
 }
