d586f752f569a026b1270ef6aacb72e1a9e7fa63
Will <will.green98@hotmail.com>
2018-02-21 01:22:38 +0000

    Week 18 Practical Music Album Set & Get

--- T2 Week 18/src/practical/musicOrganiser/Album.java
+++ T2 Week 18/src/practical/musicOrganiser/Album.java
@@ -15,20 +15,32 @@ public class Album {
         artist = albumArtist;
         label = albumLabel;
     }
-
+    
     public String getTitle() {
-        return title;
-    }
-        
-    public String getArtist() {
-        return artist;
-    }
-    
-    public String getLabel() {
-        return label;
-    }
-    
-    public void printAlbumInfo() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public void printAlbumInfo() {
         System.out.println(title + " by " + artist + ". Produced by " + label);
     }
 }
