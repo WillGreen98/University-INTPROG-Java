071386317a28410654b89feed682b7ddd03f41db
Will <will.green98@hotmail.com>
2018-02-21 01:33:57 +0000

    Week 19 In-Class Zoo example upload

---
+++ T2 Week 19/src/inClass/zoo/Canine.java
@@ -0,0 +1,20 @@
+package inClass.zoo;
+
+/**
+ * @author Will
+ * In Class Week 19
+ */
+
+public class Canine extends Animal
+{
+    
+    public Canine(String animalFood, String animalHome, String animalNoise)
+    {
+        super(animalFood, animalHome, animalNoise);
+    }
+
+    public void wagTail()
+    {
+        System.out.println("Wag Wag Wag Wag");
+    }
+}
---
+++ T2 Week 19/src/inClass/zoo/Vet.java
@@ -0,0 +1,14 @@
+package inClass.zoo;
+
+/**
+ * @author Will
+ * In Class Week 19
+ */
+
+public class Vet
+{
+    public void giveShot(Animal a)
+    {
+        a.makeNoise();
+    }
+}
