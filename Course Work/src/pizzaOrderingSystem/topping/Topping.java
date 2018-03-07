7b162daa10b8c84259a9e180a4b8f8251a1cb75a
Will <will.green98@hotmail.com>
2018-03-07 00:31:10 +0000

    Topping Super Class Upload

---
+++ Course Work/src/pizzaOrderingSystem/topping/Topping.java
@@ -0,0 +1,46 @@
+package pizzaOrderingSystem.topping;
+
+/**
+ * Super-Class to manage the pizza toping.
+ * @author 853829
+ */
+
+import pizzaOrderingSystem.Canvas;
+
+public class Topping {
+	protected Canvas canvas;
+	protected int x;
+	protected int y;
+	protected float price;
+	
+	public Topping(Canvas canvas, int x, int y, Float price) {
+		this.canvas = canvas;
+		this.x = x;
+		this.y = y;
+		this.price = price;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+}
