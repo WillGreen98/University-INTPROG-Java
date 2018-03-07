2fe91a1eb815edf467a88c635b54166315f35aba
Will <will.green98@hotmail.com>
2018-03-07 00:32:03 +0000

    Topping Class -> Anchovy

---
+++ Course Work/src/pizzaOrderingSystem/topping/Anchovy.java
@@ -0,0 +1,15 @@
+package pizzaOrderingSystem.topping;
+
+/**
+ * Toping Class to manage pizza toping Anchovy.
+ * @author 853829
+ */
+
+import pizzaOrderingSystem.Canvas;
+
+public class Anchovy extends Topping {
+	public Anchovy(Canvas canvas, int x, int y, Float price) {
+		super(canvas, x, y, price);
+		
+	}
+}
