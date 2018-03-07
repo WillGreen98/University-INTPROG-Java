99e93b34e7420a05e73c4c3e92733edffc657dae
Will <will.green98@hotmail.com>
2018-03-07 00:31:39 +0000

    Topping Class -> Bacon

---
+++ Course Work/src/pizzaOrderingSystem/topping/Bacon.java
@@ -0,0 +1,14 @@
+package pizzaOrderingSystem.topping;
+
+/**
+ * Toping Class to manage pizza toping Bacon.
+ * @author 853829
+ */
+
+import pizzaOrderingSystem.Canvas;
+
+public class Bacon extends Topping {
+	public Bacon(Canvas canvas, int x, int y, Float price) {
+		super(canvas, x, y, price);
+	}
+}
