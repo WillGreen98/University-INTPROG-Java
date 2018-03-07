3a9bbf3be55f77a5da10aa24bd2a074a1cd8b9f4
Will <will.green98@hotmail.com>
2018-03-07 01:21:21 +0000

    Draw Topping Function

--- Course Work/src/pizzaOrderingSystem/topping/Bacon.java
+++ Course Work/src/pizzaOrderingSystem/topping/Bacon.java
@@ -6,9 +6,33 @@ package pizzaOrderingSystem.topping;
  */
 
 import pizzaOrderingSystem.Canvas;
+import java.awt.Color;
+import java.awt.Rectangle;
 
 public class Bacon extends Topping {
-	public Bacon(Canvas canvas, int x, int y, Float price) {
+	private float price = 0.02f;
+	
+	public Bacon(Canvas canvas, int x, int y, float price) {
 		super(canvas, x, y, price);
 	}
+	
+	public Color setColour(Color current_Colour, Color color) {
+		if(current_Colour == color) {
+			color = Color.PINK;
+		} else {
+			color = Color.RED;
+		}
+		
+		return color;
+	} 
+	
+	public void draw_Topping() {
+		int width = 1;
+		int height = 2;
+		Color color = setColour(current_Colour, color);
+
+		for(int i=0; i<=3; i++) {
+			
+		}
+	} 
 }
