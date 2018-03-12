a8c1aaa3a518717060d8c8482990314ab435c3f8
Will <will.green98@hotmail.com>
2018-03-12 11:16:56 +0000

    Week 21 Practical Upload

---
+++ T2 Week 21/src/practical/cat/CatMain.java
@@ -0,0 +1,12 @@
+package practical.cat;
+
+public class CatMain 
+{
+    public static void main(String[] args) 
+    {
+        Cat cat1 = new Cat();
+        cat1.drawCat();
+        
+    }
+    
+}
