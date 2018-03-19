a998f941dd7ed5ca88fda211eb99f905a33d91ab
Will <will.green98@hotmail.com>
2018-03-19 16:05:51 +0000

    Anchovy Formatting & Calcs

--- Course Work/src/pizzaOrderingSystem/topping/Anchovy.java
+++ Course Work/src/pizzaOrderingSystem/topping/Anchovy.java
@@ -17,23 +17,29 @@ public class Anchovy extends Topping {
 	}
 	
 	public void draw_Topping() {
-		double circleP1X = x + 95;
-		double circleP1Y = y + 105;
 		double width = 30;
 		double height = 12;
-		double eyeX = x + 118;
-		double eyeY = y + 111;
-		double eyeDiameter = 4;
-		double TriangleP1X = x + 89;
-		double TriangleP1Y = y + 102;
+		
+		double circle_X = x + 95;
+		double circle_Y = y + 105;
+		
+		double eye_X = x + 118;
+		double eye_Y = y + 111;
+		double eye_Diameter= 4;
+		
+		double Triangle_X = x + 89;
+		double Triangle_Y = y + 102;
         
-		canvas.setForegroundColor(Color.black);
-		canvas.fillSemiCircle(circleP1X, circleP1Y, width, height, true, false);
-		canvas.setForegroundColor(Color.black);
-		canvas.fillSemiCircle(circleP1X, circleP1Y, width, height, true, true);
-		canvas.setForegroundColor(Color.white);
-		canvas.fillCircle(eyeX, eyeY, eyeDiameter);
-		canvas.setForegroundColor(Color.black);
-		canvas.fillTriangle(TriangleP1X ,TriangleP1Y, TriangleP1X, TriangleP1Y +15, TriangleP1X +15, TriangleP1Y +10);
+		canvas.setForegroundColor(Color.GRAY);
+		canvas.fillSemiCircle(circle_X, circle_Y, width, height, true, false);
+		
+		canvas.setForegroundColor(Color.GRAY);
+		canvas.fillSemiCircle(circle_X, circle_Y, width, height, true, true);
+		
+		canvas.setForegroundColor(Color.WHITE);
+		canvas.fillCircle(eye_X, eye_Y, eye_Diameter);
+		
+		canvas.setForegroundColor(Color.GRAY);
+		canvas.fillTriangle(Triangle_X ,Triangle_Y, Triangle_X, Triangle_Y +15, Triangle_X +15, Triangle_Y +10);
 	} 
 }
